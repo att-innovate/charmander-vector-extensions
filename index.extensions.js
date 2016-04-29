c39a5629d816c03e1e11825b8cee9c87acf5e79b
hc000 <hc000@users.noreply.github.com>
2016-04-29 15:17:36 -0700

    updated to support charmander and torc

--- index.extensions.js
+++ index.extensions.js
@@ -1,25 +1,39 @@
 (function () {
     'use strict';
 
-    var containerNameResolver = function($http) {
+    var containerNameResolver = function($http, $log, $rootScope) {
         var idMap = {};
-
+        var invalid = {};
+        var torc = false;
+        
         var resolve = function (id, instanceName) {
-            //console.log('instanceName:',instanceName);
             if (idMap[id]){
                 return idMap[id];
             } else {
-                var url = 'urlhere';
-                $http.get(url)
-                .then(function(response){
-                    if (response !== '' )
-                        idMap[id]=response.data.result;
-                })
-                .catch(function(){
-                    console.log('unexpected results from resolver');
-                });
-                if (instanceName.values[instanceName.values.length-1].y.indexOf('mesos') !==- 1)
-                    return id;
+                if (!invalid[id]){
+                    var url = 'http://' + $rootScope.properties.host + ':' + '31300' + '/getid/';
+                    if (torc){
+                        url = 'http://' + 'someurlhere' + ':' + '3000' + '/s?id=';
+                    }
+                    $http.get(url+id)
+                    .then(function(response){
+                        if (torc){
+                            var result = response.data.result;
+                            if (result !== '' ){
+                                idMap[id]=result;
+                            }
+                        } else {
+                            idMap[id]=response.data;
+                        }
+                    })
+                    .catch(function(){
+                        $log.debug('Unexpected response from resolver');
+                        invalid[id]=true;
+                    });
+                    if (instanceName.values[instanceName.values.length-1].y.indexOf('mesos') !==- 1){
+                        return id;
+                    }
+                }
             }
         };
 
