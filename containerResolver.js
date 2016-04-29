783efdfcc3acfa020d516a973d6a83afd540a1e3
hc000 <hc000@users.noreply.github.com>
2016-04-28 17:41:16 -0700

    Update containerResolver.js

--- containerResolver.js
+++ containerResolver.js
@@ -1,12 +1,35 @@
-'use strict';
-function containerResolver(id, hostProperties){
-	var deferred = $.Deferred();
-	$.get('http://' + hostProperties.host + ':' + '31300' +'/getid/'+id)
-	.success(function(response){
-	        deferred.resolve(response);
-	})
-	.error(function(){
-	        deferred.resolve(id.substring(0,12));
-	});
-	return deferred.promise();
-}
+(function () {
+    'use strict';
+
+    var containerNameResolver = function($http) {
+        var idMap = {};
+
+        var resolve = function (id, instanceName) {
+            //console.log('instanceName:',instanceName);
+            if (idMap[id]){
+                return idMap[id];
+            } else {
+                var url = 'urlhere;
+                $http.get(url)
+                .then(function(response){
+                    if (response !== '' )
+                        idMap[id]=response.data.result;
+                })
+                .catch(function(){
+                    console.log('unexpected results from resolver');
+                });
+                if (instanceName.values[instanceName.values.length-1].y.indexOf('mesos') !==- 1)
+                    return id;
+            }
+        };
+
+        return {
+          resolve: resolve
+        };
+    };
+
+    angular
+        .module('vector')
+        .factory('containerNameResolver', containerNameResolver)
+        ;
+})();
